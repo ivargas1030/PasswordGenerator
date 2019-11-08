bf844ec694bd164ee472873f1b6e35cdcfb091ba
ivyrose <iliana.vargas@northwestern.edu>
2019-11-07 18:20:54 -0600

    button and one special character array

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var person = prompt("Please enter a number between 8 and 128")
+
+var specialcharacters = ["!","\"","#","$","%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/" ,":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_" , "`", "{", "|", "}", "~"];
+
+function buttonClick() {
+    alert("I work!")
+    var numberpassword = prompt("Please enter a number between 8 and 128")
+}
